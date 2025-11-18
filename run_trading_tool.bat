9e8ed7a384976dca5ed68357f74982977926ac69
TSchrift <schriftt@gmail.com>
2025-11-18 00:18:35 -0500

    adding the bat file

---
+++ run_trading_tool.bat
@@ -0,0 +1,6 @@
+@echo off
+REM Change to your project folder
+cd /d "C:\Users\Schri\PycharmProjects\OptionsEval"
+
+REM Use your project virtualenv Python to run the GUI
+".venv\Scripts\python.exe" gui_launcher.py
